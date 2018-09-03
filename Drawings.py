acbe730f2f8a3400458ae46c265a3df0448cb49d
Alex H <sonicarrow@gmail.com>
2018-09-03 12:40:59 -0700

    initial commit

---
+++ Drawings.py
@@ -0,0 +1,12 @@
+# Python turtle driver/demo
+# Python 3.7
+# Using 4 spaces for indents as per:
+#   https://www.python.org/dev/peps/pep-0008/#id17
+
+from turtle import *
+
+def star():
+    print("star!")
+    
+def square():
+    print("square")
---
+++ TurtleMain.py
@@ -0,0 +1,51 @@
+# Python turtle driver/demo
+# Python 3.7
+# Using 4 spaces for indents as per:
+#   https://www.python.org/dev/peps/pep-0008/#id17
+
+# Import the module of drawing functions.
+import Drawings
+
+# When you add a function to Drawings, add a description here.
+descriptions = ["Star drawing",     # 0
+                "Square wave"]      # 1
+
+# Main menu:
+while True:
+    # Get the length of the current description index.
+    l = len(descriptions)
+
+    # Tell users what drawings we have.
+    print("\n Turtle Drawings ")
+    for i in range(l):
+        print("", i, ") ", descriptions[i])
+
+    # Ask which drawing they want to see. 
+    print("\n Which one do you want to see? (q to quit)")
+    instr = input(" > ")
+    print("")
+    
+    # Check if it's the exit condition.
+    if (instr == "q"):
+        exit()
+
+    # Now try to parse the int out of the user input.
+    # Continue the loop if they don't give us a valid number.
+    try:
+        d = int(instr)
+        if (d < 0 or d >= l):
+            print(" !!! Please give us a number in between 0 and", str(l) + ".")
+            input() # Pause to allow users to read the error message.
+            continue
+    except ValueError:
+        print(" !!! Please give us an integer.")
+        input() # Pause to allow users to read the error message.
+        continue
+
+    # Launch the right function based on what number they selected.
+    if (d == 0):
+        Drawings.star()
+    elif (d == 1):
+        Drawings.square()
+
+    # When we're done with the drawing, just loop around to the menu.
