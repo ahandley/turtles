4fb336ffb770adf10bcf50f75f52cf5897097b8c
Alex H <sonicarrow@gmail.com>
2018-09-27 19:23:47 -0700

    fixing loop bug - removing recurring menu

--- TurtleMain.py
+++ TurtleMain.py
@@ -10,42 +10,43 @@ import Drawings
 descriptions = ["Star drawing",     # 0
                 "Rainbow Spiral"]   # 1
 
-# Main menu:
-while True:
-    # Get the length of the current description index.
-    l = len(descriptions)
+# Main menu -------------------------------------
 
-    # Tell users what drawings we have.
-    print("\n Turtle Drawings ")
-    for i in range(l):
-        print("", i, ") ", descriptions[i])
+# Get the length of the current description index.
+l = len(descriptions)
 
-    # Ask which drawing they want to see. 
-    print("\n Which one do you want to see? (q to quit)")
-    instr = input(" > ")
-    print("")
-    
-    # Check if it's the exit condition.
-    if (instr == "q"):
-        exit()
+# Tell users what drawings we have.
+print("\n Turtle Drawings ")
+for i in range(l):
+    print("", i, ") ", descriptions[i])
 
-    # Now try to parse the int out of the user input.
-    # Continue the loop if they don't give us a valid number.
-    try:
-        d = int(instr)
-        if (d < 0 or d >= l):
-            print(" !!! Please give us a number in between 0 and", str(l) + ".")
-            input() # Pause to allow users to read the error message.
-            continue
-    except ValueError:
-        print(" !!! Please give us an integer.")
+# Ask which drawing they want to see. 
+print("\n Which one do you want to see? (q to quit)")
+instr = input(" > ")
+print("")
+
+# Check if it's the exit condition.
+if (instr == "q"):
+    exit()
+
+# Now try to parse the int out of the user input.
+# Continue the loop if they don't give us a valid number.
+try:
+    d = int(instr)
+    if (d < 0 or d >= l):
+        print(" !!! Please give us a number in between 0 and", str(l) + ".")
         input() # Pause to allow users to read the error message.
         continue
+except ValueError:
+    print(" !!! Please give us an integer.")
+    input() # Pause to allow users to read the error message.
+    continue
 
-    # Launch the right function based on what number they selected.
-    if (d == 0):
-        Drawings.star()
-    elif (d == 1):
-        Drawings.rainbow_spiral()
+# Launch the right function based on what number they selected.
+if (d == 0):
+    Drawings.star()
+elif (d == 1):
+    Drawings.rainbow_spiral()
 
-    # When we're done with the drawing, just loop around to the menu.
+# And now we're done!  Exit gracefully.
+exit()
